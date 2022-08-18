8f39713978067115760fd0f66be27f9e1a6217fa
williamsouef <williamsouef@gmail.com>
2022-08-18 17:16:39 +0200

    Initial Commit

---
+++ TestProjetUI/TestProjetUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjetUIApp.swift
+//  TestProjetUI
+//
+//  Created by William Souef on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestProjetUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
